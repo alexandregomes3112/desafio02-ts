97106e1159f3fd816cdcf528bfdc0b1f1e48a4c3
Alexandre Gomes <alexandre.gomes.3112@gmail.com>
2025-11-19 00:21:21 -0300

    Challenge 2 done! ✅

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,44 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
+import { Box, Center, Input, Text } from "@chakra-ui/react"
+import { Send } from "./Button";
+
+interface LoginCardProps {
+  id: string;
+  email: string;
+  password: string;
 }
 
-export const Card = ({ id, paragraph, details }: ICard) => {
-  return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+export const LoginCard = ({ id, email, password }: LoginCardProps) => {
+  return (
+    <Box minHeight='100vh' backgroundColor='#060010' padding='25px'>
+      <Box backgroundColor='#191919'  padding='15px' >
+        <Center mb="20px" >
+        <Text fontSize="2xl" fontWeight="normal" color="#554a00">
+          {id}
+        </Text>
+        </Center>
+      <Input 
+        placeholder={email}
+        type="email"
+        marginBottom="10px"
+        backgroundColor="#323232"
+        color="white"
+        borderColor="gray.600"
+        _placeholder={{ color: "gray.400" }}
+        focusBorderColor="gray.500"
+      />
+      <Input 
+        placeholder={password}
+        type="password"
+        backgroundColor="#323232"
+        color="white"
+        borderColor="gray.600"
+        _placeholder={{ color: "gray.400" }}
+        focusBorderColor="gray.500"
+      />
+      <Center marginTop="20px">
+        <Send />
+      </Center>
+      </Box>
+    </Box>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css'
 export const Header  = () => {
   return(
     <div className='header'>
-      Dio Bank
+      🏦 DIO Bank 🏦
     </div>
   )
 }
